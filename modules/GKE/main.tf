386d9bfb34d608aa270dc7f5a44f304ee64405b1
Mayur-1999 <102725772+Mayur-1999@users.noreply.github.com>
2023-07-11 13:48:25 +0530

    Update main.tf

--- modules/GKE/main.tf
+++ modules/GKE/main.tf
@@ -1,5 +1,6 @@
 resource "google_container_cluster" "test_cluster" {
   name     = var.cluster_name
+  project  = var.project_id
   # node_locations = ["us-central1-a", "us-central1-b", "us-central1-c"]
   remove_default_node_pool = true
   initial_node_count       = var.initial_node_count
@@ -61,6 +62,7 @@ resource "google_container_cluster" "test_cluster" {
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = google_container_cluster.test_cluster.name
+  project    = var.project_id
   location   = var.location
   cluster    = google_container_cluster.test_cluster.name
   node_count = var.node_count
@@ -81,33 +83,3 @@ resource "google_container_node_pool" "primary_nodes" {
 data "google_project" "project" {
   project_id = var.project_id
 }
-
-/*
- ##enable GKEbackup API
-resource "google_project_service" "backup_api" {
-  project = var.project_id
-  service = "gkebackup.googleapis.com"
-  disable_on_destroy = false
-}
-
-# Backup plan for GKE , needs to be configured according to our use 
-resource "google_gke_backup_backup_plan" "backup_plan" {
-  name = "full-backup-plan"
-  cluster = google_container_cluster.sre_cluster.id
-  depends_on = [google_container_node_pool.primary_nodes]
-  location = var.location
-  retention_policy {
-     backup_delete_lock_days = 30
-     backup_retain_days = 180
-  }
-  backup_schedule {
-     cron_schedule = "5 4 30 2 3"
-  }
-  backup_config {
-    include_volume_data = true
-    include_secrets = true
-    all_namespaces = true
-  }
-  
-}
-*/
